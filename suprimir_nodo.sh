f16f5a87fe4b8cda3040602ac8a65cccaff2338e
Frank Josué Vigil Vega <frankjosue.vigilvega@gmail.com>
2022-09-05 17:19:41 +0200

    agregando eliminar nodo en puppetserver y puppetdb

--- suprimir_nodo.sh
+++ suprimir_nodo.sh
@@ -1,4 +1,11 @@
+echo "El parametro node es: \n"
 echo $1
 
-echo "Ejecutando desde el servidor puppetserver en docker"
-docker exec -it puppet puppetserver ca list --all
+echo "Elimación del node en Puppetdb"
+docker exec -it puppet puppet node deactivate $1
+
+
+echo "Elimación del node en Puppetserver"
+docker exec -it puppet puppetserver ca clean $1
+
+
